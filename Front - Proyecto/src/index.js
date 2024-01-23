46088c008468896dd782e6e985958f2cb9a25d8b
Odeth Arevalo <odeth.arevalo@utp.ac.pa>
2024-01-23 11:46:32 -0500

    Actualización Auth
    Flujo de autenticación listo, falta unirlo al backend

--- Front - Proyecto/src/index.js
+++ Front - Proyecto/src/index.js
@@ -98,3 +98,155 @@ document.getElementById('pub').addEventListener('mouseleave', function () {
 this.querySelector('.absolute').classList.add('hidden');
 });
 
+function mostrarInicioS() {
+  document.body.style.overflow = 'hidden';
+
+  var overlay = document.getElementById('login');
+  var loginContainer = document.getElementById('contenedor-login');
+  var loginContent = document.getElementById('login-contenido');
+
+  // Cargar dinámicamente el contenido
+  fetch('/Front - Proyecto/src/Auth/login.html')
+    .then(response => response.text())
+    .then(data => {
+      loginContent.innerHTML = data;
+      overlay.style.display = 'block';
+      
+      loginContainer.style.display = 'flex';
+    })
+    .catch(error => console.error(error));
+}
+
+function mostrarRegistro(event) {
+  event.preventDefault();
+  document.body.style.overflow = 'hidden';
+
+  var overlay = document.getElementById('registro');
+  var registroContainer = document.getElementById('contenedor-registro');
+  var registroContent = document.getElementById('registro-contenido');
+
+  cerrarLogin();
+  // Cargar dinámicamente el contenido del perfil desde pantallaPerfil.html
+  fetch('/Front - Proyecto/src/Auth/Registro.html')
+    .then(response => response.text())
+    .then(data => {
+      registroContent.innerHTML = data;
+      overlay.style.display = 'block';
+      
+      registroContainer.style.display = 'flex';
+    })
+    .catch(error => console.error(error));
+}
+
+function mostrarR1() {
+  document.body.style.overflow = 'hidden';
+
+  var overlay = document.getElementById('reestablecer1');
+  var rpContainer = document.getElementById('contenedor-reestablecer1');
+  var rpContent = document.getElementById('reestablecer1-contenido');
+
+  cerrarLogin();
+  // Cargar dinámicamente el contenido
+  fetch('/Front - Proyecto/src/Auth/Reestablecer_contraseña.html')
+    .then(response => response.text())
+    .then(data => {
+      rpContent.innerHTML = data;
+      overlay.style.display = 'block';
+      
+      rpContainer.style.display = 'flex'; 
+    })
+    .catch(error => console.error(error));
+}
+
+
+function mostrarR2() {
+  document.body.style.overflow = 'hidden';
+
+  var overlay = document.getElementById('reestablecer2');
+  var rp2Container = document.getElementById('contenedor-reestablecer2');
+  var rp2Content = document.getElementById('reestablecer2-contenido');
+
+  cerrarR1();
+  // Cargar dinámicamente el contenido
+  fetch('/Front - Proyecto/src/Auth/Revisa_tu_bandeja.html')
+    .then(response => response.text())
+    .then(data => {
+      rp2Content.innerHTML = data;
+      overlay.style.display = 'block';
+      
+      rp2Container.style.display = 'flex'; 
+    })
+    .catch(error => console.error(error));
+}
+
+function mostrarR3() {
+  document.body.style.overflow = 'hidden';
+
+  var overlay = document.getElementById('reestablecer3');
+  var rp3Container = document.getElementById('contenedor-reestablecer3');
+  var rp3Content = document.getElementById('reestablecer3-contenido');
+
+  cerrarLogin();
+  // Cargar dinámicamente el contenido
+  fetch('/Front - Proyecto/src/Auth/Reestablezca_contraseña.html')
+    .then(response => response.text())
+    .then(data => {
+      rp3Content.innerHTML = data;
+      overlay.style.display = 'block';
+      
+      rp3Container.style.display = 'flex'; 
+    })
+    .catch(error => console.error(error));
+}
+
+
+function cerrarRegistro() {
+  document.body.style.overflow = 'auto';
+
+  var overlay = document.getElementById('registro');
+  var registroContainer = document.getElementById('contenedor-registro');
+
+  overlay.style.display = 'none';
+  registroContainer.style.display = 'none';
+}
+
+function cerrarLogin() {
+  document.body.style.overflow = 'auto';
+
+  var overlay = document.getElementById('login');
+  var loginContainer = document.getElementById('contenedor-login');
+
+  overlay.style.display = 'none';
+  loginContainer.style.display = 'none';
+}
+
+function cerrarR1() {
+  document.body.style.overflow = 'auto';
+
+  var overlay = document.getElementById('reestablecer1');
+  var rpContainer = document.getElementById('contenedor-reestablecer1');
+
+  overlay.style.display = 'none';
+  rpContainer.style.display = 'none';
+}
+
+function cerrarR2() {
+  document.body.style.overflow = 'auto';
+
+  var overlay = document.getElementById('reestablecer2');
+  var rp2Container = document.getElementById('contenedor-reestablecer2');
+
+  overlay.style.display = 'none';
+  rp2Container.style.display = 'none';
+}
+
+function cerrarR3() {
+  document.body.style.overflow = 'auto';
+
+  var overlay = document.getElementById('reestablecer3');
+  var rp3Container = document.getElementById('contenedor-reestablecer3');
+
+  overlay.style.display = 'none';
+  rp3Container.style.display = 'none';
+}
+
