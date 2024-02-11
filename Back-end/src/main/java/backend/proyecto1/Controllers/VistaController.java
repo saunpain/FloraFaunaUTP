c6efe6de6b6e2f4b191f9d6af4d92fe35d27bb12
Nadim <nurolink14@gmail.com>
2024-02-11 17:31:24 -0500

    Filtros terminados

--- Back-end/src/main/java/backend/proyecto1/Controllers/VistaController.java
+++ Back-end/src/main/java/backend/proyecto1/Controllers/VistaController.java
@@ -3,6 +3,7 @@ package backend.proyecto1.Controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
+
 import backend.proyecto1.Services.VistaDb;
 import backend.proyecto1.Models.VistaPubGlobal;
 
@@ -18,20 +19,32 @@ public class VistaController {
     public VistaPubGlobal ObtenerPublicacion(@PathVariable ("id_publicacion") int id){
         return new VistaDb().ObtenerPublicacion(id);
     }
+
     @GetMapping("vista_flora")
     public List<VistaPubGlobal> VistaPublicacionesFlora(){
-        return new VistaDb().VistaPublicacionesFlora();
+        return
+         new VistaDb().VistaPublicacionesFlora();
     }
+
     @GetMapping("vista_fauna")
     public List<VistaPubGlobal> VistaPublicacionesFauna(){
         return new VistaDb().VistaPublicacionesFauna();
     }
+    
     @GetMapping("/vista_flora/{categoria_flora}")
     public List<VistaPubGlobal> ObtenerFloraCategoria(@PathVariable ("categoria_flora") String cat){
         return new VistaDb().VistaFloraCat(cat);
     }
+
     @GetMapping("/vista_fauna/{categoria_fauna}")
     public List<VistaPubGlobal> ObtenerFaunaCategoria(@PathVariable ("categoria_fauna") String cat){
         return new VistaDb().VistaFaunaCat(cat);
+    }
+
+    @GetMapping("/vista/filtrarGlob")
+    public List<VistaPubGlobal> FiltrarPublicacionesGlob(
+        @RequestParam(required = true) String busqueda
+    ){
+        return new VistaDb().FiltrarPublicaciones(busqueda);
     }
 }
